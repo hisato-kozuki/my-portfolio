06f9f7de1eb184cdace431d829721fec768428a1
hisato-kozuki <167668521+hisato-kozuki@users.noreply.github.com>
2024-04-22 09:34:09 +0000

    summary of your commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
-import Link from "next/link";
+import '@/app/paper/paper.css'
 
-export default function Home() {
-  return (
-      <main className={'w-screen h-screen p-20 bg-slate-50 flex flex-row gap-4'}>
-          <Link href={'/orange'}>
-              Orange
-          </Link>
-          <Link href={'/paper'}>
-              Paper
-          </Link>
-          <Link href={'/photographic'}>
-              Photographic
-          </Link>
-      </main>
-  );
+export default function Page() {
+    return (
+        <div className={'bg-paper-background w-screen min-h-screen text-center'}>
+            <header className={'absolute w-screen px-12 py-6 flex gap-8 justify-end'}>
+                <a>About</a>
+                <a>Works</a>
+            </header>
+            <main className={'inline-block mx-auto'}>
+                <div className={'paper'}>
+                    <h1>Anya Forger</h1>
+                    <h2>アーニャ・フォージャー</h2>
+                    <h3>About</h3>
+                    <p>○○大学○○学部○○学科の大学○年生．好きなものは○○，嫌いなものは○○．</p>
+                    <h3>History</h3>
+                    <h3>Contact</h3>
+                    <h3>Works</h3>
+                </div>
+            </main>
+            <footer className={'py-8'}>
+                anya © {new Date().getFullYear()}
+            </footer>
+        </div>
+    )
 }
