466dde8b9ef7b675850970f999f95222666fa064
hisato-kozuki <167668521+hisato-kozuki@users.noreply.github.com>
2024-04-22 18:50:44 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import '@/app/paper/paper.css'
 
 export default function Page() {
-    return (<body>
+    return (
         <div className={'bg-paper-background w-screen min-h-screen text-center'}>
             <header className={'absolute w-screen px-12 py-6 flex gap-8 justify-end'}>
                 <a>About</a>
@@ -10,10 +10,10 @@ export default function Page() {
             <main className={'inline-block mx-auto'}>
                 <div className={'paper'}>
                     <h1>Ar</h1>
-                    <h2>村部遥紀</h2>
+                    <h2>村部遥紀</h2><div className={'body'}>
                     <h3>About</h3>
                     <p>○○大学○○学部○○学科の大学○年生．好きなものは○○，嫌いなものは○○．</p>
-                    <h3>History</h3>
+                    <h3>History</h3></div>
                     <h3>Contact</h3>
                     <h3>Works</h3>
                 </div>
@@ -21,6 +21,6 @@ export default function Page() {
             <footer className={'py-8'}>
                 anya © {new Date().getFullYear()}
             </footer>
-        </div></body>
+        </div>
     )
 }
