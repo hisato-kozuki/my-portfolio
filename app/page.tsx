f5c2f054ea70cef2d02af4b6bf6957175d43bea9
hisato-kozuki <167668521+hisato-kozuki@users.noreply.github.com>
2024-04-22 09:47:30 +0000

    anme

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Page() {
             </header>
             <main className={'inline-block mx-auto'}>
                 <div className={'paper'}>
-                    <h1>Anya Forger</h1>
-                    <h2>アーニャ・フォージャー</h2>
+                    <h1>Ar</h1>
+                    <h2>村部遥紀</h2>
                     <h3>About</h3>
                     <p>○○大学○○学部○○学科の大学○年生．好きなものは○○，嫌いなものは○○．</p>
                     <h3>History</h3>
