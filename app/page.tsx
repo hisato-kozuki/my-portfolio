bd8e8fbf995c49e577097e7af4df3cda19baf5ec
hisato_kozuki <q66yuan@gmail.com>
2024-12-06 13:04:52 +0900

    spreadsheet function

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             <main className={'inline-block mx-auto'}>
                 <div className={'paper'}>
                     <h1>Ar</h1>
-                    <h2>村部遥紀</h2><div className={'lime'}>
+                    <h2>村部遥紀</h2><div className={'lime'} onclick="function1()">
                     <h3>About</h3>
                     <p>○○大学○○学部○○学科の大学○年生．好きなものは○○，嫌いなものは○○．</p>
                     <h3>History</h3></div>
@@ -23,4 +23,24 @@ export default function Page() {
             </footer>
         </div>
     )
+}
+const url = "file:///C:/Users/r1192/Desktop/reversi%20memory%20game%20desktop.html"; // GASで取得したウェブアプリのURL
+const data = {
+    'name': '山田 太郎',
+    'email': 'yamada@example.com',
+    'message': 'こんにちは！'
+};
+const options = {
+    'method' : 'post',
+    'headers': {
+        'Content-Type': 'application/json'
+    },
+    'payload' : JSON.stringify(data) //送りたいデータをpayloadに配置してJSON形式変換。
+};
+function function1(){
+    //res = UrlFetchApp.fetch(url,options); // <- Post リクエスト
+    fetch(url, options)
+.then(response => response.text())
+.then(data => console.log(data))
+.catch(error => console.error("Error:", error));
 }
